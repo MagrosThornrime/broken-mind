9c97d5c2cbb68fdc6130e7dcd77df23f55e4900c
MagrosThornrime <magros.thornrime@gmail.com>
2025-06-09 03:40:26 +0200

    Start game maximised

--- broken-mind/scripts/player.gd
+++ broken-mind/scripts/player.gd
@@ -42,14 +42,15 @@ func _play_walk():
 func _physics_process(_delta: float) -> void:
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	if input_direction[0] > 0:
-		direction = MOVE_DIRECTION.right
-	elif input_direction[0] < 0:
-		direction = MOVE_DIRECTION.left
-	elif input_direction[1] < 0:
+	if input_direction[1] < 0:
 		direction = MOVE_DIRECTION.front
 	elif input_direction[1] > 0:
 		direction = MOVE_DIRECTION.back
+	elif input_direction[0] > 0:
+		direction = MOVE_DIRECTION.right
+	elif input_direction[0] < 0:
+		direction = MOVE_DIRECTION.left
+
 	
 	if !inviolable:
 		_play_walk()
